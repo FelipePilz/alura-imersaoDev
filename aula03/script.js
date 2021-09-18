54686bdeca159c3b5958719d5f00aee17614f6a7
FelipePilz <felipevilvertpilz@gmail.com>
2021-09-18 17:22:50 -0300

    commit on aula03

---
+++ aula03/script.js
@@ -0,0 +1,7 @@
+
+//elementoResultado.innerHTML
+
+  function Chutar() {
+       let numeroSecreto = Math.floor(Math.random() * 11); var numeroChutado = parseInt(document.getElementById("valor").value);  
+  }
+

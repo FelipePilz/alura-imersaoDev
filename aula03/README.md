2f7dbe18eade7696223143f669642a40bbe1b2a3
FelipePilz <felipevilvertpilz@gmail.com>
2021-09-19 00:47:42 -0300

    commit on master

---
+++ aula03/README.md
@@ -0,0 +1,11 @@
+Criar uma jogo de adivinhação, onde o programa escolhe um número aleatório e nós vamos tentar acertar.
+
+- Fazer testes utilizando console.log();
+- Criar a lógica por trás do "chute" com if, else if e else;
+- Utilizar a função Math.random() do JavaScript para gerar números aleatórios;
+
+Desafios:
+
+-Adicionar um número de tentativas para a pessoa tentar acertar e imprimir a resposta no final;
+-Quando a pessoa errar, deixar na mensagem se o número chutado é maior ou menor que o número secreto;
+-Pesquisar e aprender a diferença entre == e ===;
--- aula03/script.js
+++ aula03/script.js
@@ -1,8 +1,12 @@
 var tentativas = 1;
 var numeroSecreto = parseInt(Math.floor(Math.random() * 11));
+var repetir = 0;
 
-  function chutar() {
-     
+function chutar() {
+     if(repetir == 1){
+          alert("Você já descobriu o número aleatório!");
+          return;
+     }
      let numeroChutado = parseInt(document.getElementById("valor").value);
 
      while(numeroChutado < 0 || numeroChutado > 10 || isNaN(numeroChutado)){
@@ -11,17 +15,19 @@ var numeroSecreto = parseInt(Math.floor(Math.random() * 11));
      }
      
      let elementoResultado = document.getElementById("resultado");
+
+
      while(numeroChutado != numeroSecreto){
           tentativas ++;
           if(numeroChutado < numeroSecreto){
                elementoResultado.innerHTML = "Tente um número maior!";
                return;
           }else{
-               elementoResultado.innerHTML = "Tente um número menor";
+               elementoResultado.innerHTML = "Tente um número menor!";
                return;
           }
           
      }
+     repetir = 1;
      elementoResultado.innerHTML = "Você descobriu o número em "+tentativas+" tentativas!";
-     }
-
+}

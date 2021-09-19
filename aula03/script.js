93fe3152c76991dd888f92513973c72b4e4e931c
FelipePilz <felipevilvertpilz@gmail.com>
2021-09-18 21:43:18 -0300

    commit on aula03

--- aula03/script.js
+++ aula03/script.js
@@ -2,6 +2,14 @@
 //elementoResultado.innerHTML
 
   function Chutar() {
-       let numeroSecreto = Math.floor(Math.random() * 11); var numeroChutado = parseInt(document.getElementById("valor").value);  
-  }
+     let numeroSecreto = Math.floor(Math.random() * 11);
+     let numeroChutado = parseInt(document.getElementById("valor").value);
+
+     while(numeroChutado < 0 || numeroChutado > 10 || isNaN(numeroChutado)){
+          alert("Insira um valor valido!");
+          return;
+     }
+     
+  
+     }
 

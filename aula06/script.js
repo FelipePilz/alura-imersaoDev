7ffdfb0c7b868b95298690d86e31deec41113bcf
FelipePilz <felipevilvertpilz@gmail.com>
2021-09-21 22:17:24 -0300

    commit on master

--- aula06/script.js
+++ aula06/script.js
@@ -1,15 +1,13 @@
 var informacoesJogadores= [{
-    nome: 'Liquid',
-    avatar: 'https://img-cdn.hltv.org/teamlogo/JMeLLbWKCIEJrmfPaqOz4O.svg?ixlib=java-2.1.0&s=c02caf90234d3a3ebac074c84ba1ea62',
+    nome: 'Lelis',
+    avatar: 'https://piratebay-coaching.com/wp-content/uploads/2019/08/lelis-perfil-Rodrigo-Santos.png',
     vitorias: 0,
-    empates: 0,
     derrotas: 0,
     pontos: 0
 },{
-    nome: 'SG ESports',
-    avatar: 'https://pbs.twimg.com/profile_images/1355306358002872326/LKjDmosb_400x400.jpg',
+    nome: 'Ceb',
+    avatar: 'https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2019/08/02/acbbbab4-afce-4b7a-aed1-d5d0e1e36381/sebastien-ceb-debs-og-dota',
     vitorias: 0,
-    empates: 0,
     derrotas: 0,
     pontos: 0
 }];
@@ -20,11 +18,9 @@ function exibeJogadorTela(informacoesJogadores){
         elemento += "<tr>";
         elemento += "<td>"+informacoesJogadores[i].nome+"<br><img class='fotoPerfil' src='"+informacoesJogadores[i].avatar+"'></td>";
         elemento += "<td>"+informacoesJogadores[i].vitorias+"</td>";
-        elemento += "<td>"+informacoesJogadores[i].empates+"</td>";
         elemento += "<td>"+informacoesJogadores[i].derrotas+"</td>";
         elemento += "<td>"+calculaPontos(informacoesJogadores[i])+"</td>";
         elemento += "<td><button onClick='adicionarVitoria("+i+")'>Vitória</button></td>";
-        elemento += "<td><button onClick='adicionarEmpate("+i+")'>Empate</button></td>";
         elemento += "<td><button onClick='adicionarDerrota("+i+")'>Derrota</button></td>";
         elemento += "</tr>";
     }
@@ -35,17 +31,17 @@ function exibeJogadorTela(informacoesJogadores){
 }
 
 function calculaPontos(jogador){
-    var pontos = parseInt((jogador.vitorias * 3) + jogador.empates);
+    var pontos = parseInt((jogador.vitorias * 3) - jogador.derrotas);
     return pontos;
 }
 
 function adicionarJogador(){
-    if(confirm("Você quer adicionar um jogador?") == true){
+    if(confirm("Você quer adicionar um Jogador?") == true){
         informacoesJogadores.push({
-            nome: prompt("Informe o nome do jogador:"),
-            vitorias: parseInt(prompt("Quantas vitorias ele teve?")),
-            empates: parseInt(prompt("Quantos empates ele teve?")),
-            derrotas: parseInt(prompt("Quantas derrotas ele teve?")),
+            nome: prompt("Informe o nome do Jogador:"),
+            avatar: prompt("Insira o link da imagem dele:"),
+            vitorias: validaVitorias(prompt("Quantas vitorias ele teve?"), 1),
+            derrotas: validaDerrotas(prompt("Quantas derrotas ele teve?"), 1),
             pontos: 0
         })
         exibeJogadorTela(informacoesJogadores)
@@ -56,18 +52,64 @@ function adicionarJogador(){
 
 function adicionarVitoria(index){
     informacoesJogadores[index].vitorias += 1;
-    console.log(informacoesJogadores[index].vitorias, informacoesJogadores[index].nome)
+    validaVitorias(index, 0);
     exibeJogadorTela(informacoesJogadores);
-}
-
-function adicionarEmpate(index){
-    informacoesJogadores[index].empates += 1;
-    console.log(informacoesJogadores[index].empates, informacoesJogadores[index].nome)
-    exibeJogadorTela(informacoesJogadores);
+    
 }
 
 function adicionarDerrota(index){
     informacoesJogadores[index].derrotas += 1;
-    console.log(informacoesJogadores[index].derrotas, informacoesJogadores[index].nome)
+    validaDerrotas(index, 0);
     exibeJogadorTela(informacoesJogadores);
+}
+
+
+function validaVitorias(index, validaFuncao){ 
+    var vitoriasFaltantes = 1;
+    if(validaFuncao == 1){
+        vitoriasFaltantes = parseInt(index);
+    }
+    while(vitoriasFaltantes !=0 ){
+        for(var i = 0; i < informacoesJogadores.length; i++){
+            if(vitoriasFaltantes == 0){
+                i == informacoesJogadores.length;
+            }else{
+                if(index != i && confirm("Ele ganhou contra "+informacoesJogadores[i].nome+"?") == true){
+                    informacoesJogadores[i].derrotas ++;
+                    console.log(informacoesJogadores[i].derrotas)
+                    vitoriasFaltantes -= 1;
+                }
+            }        
+        }
+    }
+    if(validaFuncao == 1) return parseInt(index);
+}
+
+
+function validaDerrotas(index, validaFuncao){
+    var derrotasFaltantes = 1;
+    if(validaFuncao == 1){
+        derrotasFaltantes = parseInt(index);
+    }
+    while(derrotasFaltantes !=0 ){
+        for(var i = 0; i < informacoesJogadores.length; i++){
+            if(derrotasFaltantes == 0){
+                i == informacoesJogadores.length;
+            }else{
+                if(index != i && confirm("Ele perdeu contra "+informacoesJogadores[i].nome+"?") == true){
+                    informacoesJogadores[i].vitorias ++;
+                    derrotasFaltantes -= 1;
+                }
+            }        
+        }
+    }
+    if(validaFuncao == 1) return parseInt(index);    
+}
+
+function reset(){
+    for(var i = 0; i <  informacoesJogadores.length; i++){
+        informacoesJogadores[i].vitorias = 0;
+        informacoesJogadores[i].derrotas = 0;
+        exibeJogadorTela(informacoesJogadores);
+    }   
 }
